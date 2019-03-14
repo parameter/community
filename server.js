1b38ce290d0fcbb0c0d1c7f90b22f6d35bff5746
parameter <paer.henriksson@yahoo.com>
2019-03-14 17:01:13 +0100

    ...

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ require('./server/models').connect(config.dbUri);
 
 const app = express();
 // tell the app to look for static files in these directories
-app.use(express.static('./server/static/'));
+app.use(express.static('./client'));
 app.use(express.static('./client/dist/'));
 // tell the app to parse HTTP body messages
 app.use(bodyParser.json());
